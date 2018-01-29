4e33fe34582bfbf49bb15ad870f0286c1565da76
Alexander <atalexand@gmail.com>
2018-01-29 11:40:29 -0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
+
+
+var cards = ["queen","queen","king","king"];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+var cardTwo = cards[1];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardsInPlay);
+
+
+if(cardsInPlay.length === 2) {
+  if (cardsInPlay[0]===cardsInPlay[1]){
+    alert("You have a match!");
+  }
+  else{
+      alert ("Sorry Try Again.");
+  }
+};
