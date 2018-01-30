e6769262666b1671adbd35a78d326f8c5f6749c6
Alexander <atalexand@gmail.com>
2018-01-30 11:29:14 -0800

    updated game logic

--- js/main.js
+++ js/main.js
@@ -4,20 +4,19 @@ console.log("Up and running!");
 var cards = ["queen","queen","king","king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-var cardTwo = cards[1];
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardsInPlay);
-
-
-if(cardsInPlay.length === 2) {
-  if (cardsInPlay[0]===cardsInPlay[1]){
-    alert("You have a match!");
-  }
-  else{
-      alert ("Sorry Try Again.");
+var checkForMatch = function() {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    console.log("You found a match!");
+  } else {
+    console.log("Sorry, try again.");
+    alert("Sorry, try again.");
   }
 };
+var flipCard = function(cardId){
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+};
+
+flipCard(0);
+flipCard(2);
+checkForMatch();
