b471ab90fc6e273a76d93adbfe031f10c97de88f
Alexander <atalexand@gmail.com>
2018-01-30 13:09:43 -0800

    updated game logic again to include mose descriptions of cards

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 console.log("Up and running!");
 
 
-var cards = ["queen","queen","king","king"];
+var cards = [
+  {rank:"queen" ,suit:"hearts" , cardImage:"images/queen-of-hearts.png"},
+  {rank:"queen",suit:"diamonds",cardImage:"images/queen-of-diamonds.png"},
+  {rank:"king",suit:"hearts",cardImage:"images/king-of-hearts.png"},
+  {rank:"king",suit:"diamonds",cardImage:"images/king-of-diamonds.png"}
+];
 var cardsInPlay = [];
 
 var checkForMatch = function() {
@@ -13,10 +18,12 @@ var checkForMatch = function() {
   }
 };
 var flipCard = function(cardId){
-  console.log("User flipped " + cards[cardId]);
-  cardsInPlay.push(cards[cardId]);
+  console.log("User flipped " + cards[cardId].rank);
+  console.log(cards[cardId].suit);
+  console.log(cards[cardId].cardImage);
+  cardsInPlay.push(cards[cardId].rank);
 };
 
 flipCard(0);
-flipCard(2);
+flipCard(1);
 checkForMatch();
